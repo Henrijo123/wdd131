568094cfc5b1ee59f0842aa3f6ef129188a15885
Henrijo123 <henrijo123@gmail.com>
2025-05-07 23:06:56 -0300

    W01 Assignment: Home Page - Finished

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+const year = document.querySelector("#currentyear");
+const date = document.querySelector("#lastModified");
+const today = new Date();
+
+year.innerHTML = `&copy<span class="highlight">${today.getFullYear()} </span>`;
+
+date.innerHTML = `Last Modified: <span id="lastModified">${new Intl.DateTimeFormat(
+    "en-US",
+    {
+        dateStyle: "short"
+    }
+).format(today)} ${today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()}</span>`;
